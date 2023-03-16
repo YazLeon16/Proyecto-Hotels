3f7bc2e973359b51671ff766dd5f60e225b1c5fb
Yazmin Leon <leonyaz2106@gmail.com>
2023-03-15 18:19:24 -0600

    Insercion de datos

--- js/app.js
+++ js/app.js
@@ -13,25 +13,56 @@ const loadHot = () => {
             'X-RapidAPI-Key': 'e4d6169bbfmsh3ea3bd5f647c0abp140a27jsn7becb48982b6',
             'X-RapidAPI-Host': 'hotels4.p.rapidapi.com'
         }
-
-};
-
-fetch('https://hotels4.p.rapidapi.com/v2/get-meta-data', options)
-	.then(response => response.json())
-	.then(response => {
-        topHotel =response
-        tarjetas
-    })
-	.catch(err => console.error(err));
+    };
+    
+    fetch('http://localhost:5050/js/API.json', options)
+        .then(response => response.json())
+        .then(response => {
+            topHotel = response
+            console.log(topHotel)
+            tarjetas()//metodo
+        })
+        .catch(err => console.error(err));
+        
 }
     const tarjetas = () => {
-        topHotel.forEach(hotel =>{
-            console.log(hotel)
-            hotelName.querySelector('img').setAttribute('src',hotel.trackMetadata.displayImageUri)
-            hotelName.querySelectorAll('p')[0].textContent = hotel.trackMetadata.hotels[0].name
-            hotelName.querySelectorAll('p')[1].textContent = hotel.trackMetadata.hotels[1].posName
-            hotelName.querySelectorAll('p')[2].textContent = hotel.trackMetadata.hotels[2].hcomLocale
 
-            
-        })
+           //hotelName.querySelector('img').setAttribute('src',topHotel.LATAM.lastMinuteDealsCardImageUrl)
+           hotelName.querySelectorAll('p')[0].textContent = "Código del país: " + topHotel.LATAM.countryCode
+           hotelName.querySelectorAll('p')[1].textContent = "Idioma: " + topHotel.LATAM.supportedLocales[0].languageCode
+           hotelName.querySelectorAll('p')[2].textContent = "Numero telefonico: " + topHotel.LATAM.supportedLocales[0].localeSpecificSupportPhoneNumber
+           
+           hotelName.querySelectorAll('p')[3].textContent ="Codigo del país: " + topHotel.BE.countryCode
+           hotelName.querySelectorAll('p')[4].textContent = "Idioma: " + topHotel.BE.supportedLocales[0].languageCode
+           hotelName.querySelectorAll('p')[5].textContent = "Numero telefonico: " + topHotel.BE.supportedLocales[0].localeSpecificSupportPhoneNumber
+          
+           hotelName.querySelectorAll('p')[6].textContent ="Codigo del país: " + topHotel.JP.countryCode
+           hotelName.querySelectorAll('p')[7].textContent = "Idioma: " + topHotel.JP.supportedLocales[0].languageCode
+           hotelName.querySelectorAll('p')[8].textContent = "Numero telefonico: " + topHotel.JP.supportedLocales[0].localeSpecificSupportPhoneNumber
+
+           hotelName.querySelectorAll('p')[9].textContent ="Codigo del país: " + topHotel.TW.countryCode
+           hotelName.querySelectorAll('p')[10].textContent = "Idioma: " + topHotel.TW.supportedLocales[0].languageCode
+           hotelName.querySelectorAll('p')[11].textContent = "Numero telefonico: " + topHotel.TW.supportedLocales[0].localeSpecificSupportPhoneNumber
+
+           hotelName.querySelectorAll('p')[12].textContent ="Codigo del país: " + topHotel.ID.countryCode
+           hotelName.querySelectorAll('p')[13].textContent = "Idioma: " + topHotel.ID.supportedLocales[0].languageCode
+           hotelName.querySelectorAll('p')[14].textContent = "Numero telefonico: " + topHotel.ID.supportedLocales[0].localeSpecificSupportPhoneNumber
+
+           hotelName.querySelectorAll('p')[15].textContent ="Codigo del país: " + topHotel.SG.countryCode
+           hotelName.querySelectorAll('p')[16].textContent = "Idioma: " + topHotel.SG.supportedLocales[0].languageCode
+           hotelName.querySelectorAll('p')[17].textContent = "Numero telefonico: " + topHotel.SG.supportedLocales[0].localeSpecificSupportPhoneNumber           
+
+           hotelName.querySelectorAll('p')[18].textContent ="Codigo del país: " + topHotel.CA.countryCode
+           hotelName.querySelectorAll('p')[19].textContent = "Idioma: " + topHotel.CA.supportedLocales[0].languageCode
+           hotelName.querySelectorAll('p')[20].textContent = "Numero telefonico: " + topHotel.CA.supportedLocales[0].localeSpecificSupportPhoneNumber
+
+           hotelName.querySelectorAll('p')[21].textContent ="Codigo del país: " + topHotel.CO.countryCode
+           hotelName.querySelectorAll('p')[22].textContent = "Idioma: " + topHotel.CO.supportedLocales[0].languageCode
+           hotelName.querySelectorAll('p')[23].textContent = "Numero telefonico: " + topHotel.CO.supportedLocales[0].localeSpecificSupportPhoneNumber
+           hotelName.querySelectorAll('p')[24].textContent = "Reservación: " + topHotel.CO.deepLinkDateFormat
+           
+           const clone = hotelName.cloneNode(true)
+           fragment.appendChild(clone)
+
+           cardsHotel.appendChild(fragment)
 }
